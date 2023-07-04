3a742ea078d7778408d2e9ae51621316bf7ce137
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 17:48:30 -0600

    Subscribe to workoutsChanged

--- src/app/workouts/workout-list/workout-list.component.ts
+++ src/app/workouts/workout-list/workout-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Workout } from '../workout.model';
 import { WorkoutService } from '../workout.service';
@@ -7,12 +8,23 @@ import { WorkoutService } from '../workout.service';
   selector: 'app-workout-list',
   templateUrl: './workout-list.component.html',
 })
-export class WorkoutListComponent implements OnInit {
+export class WorkoutListComponent implements OnInit, OnDestroy {
   workouts: Workout[];
+  subscription: Subscription;
 
   constructor(private workoutService: WorkoutService) {}
 
   ngOnInit() {
+    // When workouts array changes, reflect change in workout list
+    this.subscription = this.workoutService.workoutsChanged.subscribe(
+      (workouts: Workout[]) => {
+        this.workouts = workouts;
+      }
+    );
     this.workouts = this.workoutService.getWorkouts();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
