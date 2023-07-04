25db6f5d98394230fda1d1aa0ee7e08b72b46335
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 17:19:24 -0600

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { GoalsComponent } from './goals/goals.component';
+import { WorkoutDetailComponent } from './workouts/workout-detail/workout-detail.component';
+import { WorkoutStartComponent } from './workouts/workout-start/workout-start.component';
+import { WorkoutEditComponent } from './workouts/workout-edit/workout-edit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/workouts', pathMatch: 'full' },
+  {
+    path: 'workouts',
+    component: WorkoutsComponent,
+    children: [
+      { path: '', component: WorkoutStartComponent },
+      { path: 'new', component: WorkoutEditComponent },
+      { path: ':id', component: WorkoutDetailComponent },
+      { path: ':id/edit', component: WorkoutEditComponent },
+    ],
+  },
+  { path: 'goals', component: GoalsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
