1e8eab80d995fce8d6b7c8c54eb206ae4f05c320
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 17:19:40 -0600

    Import components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WorkoutsComponent } from './workouts/workouts.component';
 import { HeaderComponent } from './header/header.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { WorkoutListComponent } from './workouts/workout-list/workout-list.component';
+import { WorkoutItemComponent } from './workouts/workout-list/workout-item/workout-item.component';
+import { WorkoutDetailComponent } from './workouts/workout-detail/workout-detail.component';
+import { WorkoutEditComponent } from './workouts/workout-edit/workout-edit.component';
+import { GoalsComponent } from './goals/goals.component';
+import { GoalListComponent } from './goals/goal-list/goal-list.component';
+import { GoalItemComponent } from './goals/goal-list/goal-item/goal-item.component';
+import { GoalEditComponent } from './goals/goal-edit/goal-edit.component';
+import { WorkoutStartComponent } from './workouts/workout-start/workout-start.component';
 
 @NgModule({
-  declarations: [AppComponent, WorkoutsComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    WorkoutsComponent,
+    WorkoutListComponent,
+    WorkoutItemComponent,
+    WorkoutDetailComponent,
+    WorkoutEditComponent,
+    WorkoutStartComponent,
+    GoalsComponent,
+    GoalListComponent,
+    GoalItemComponent,
+    GoalEditComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
