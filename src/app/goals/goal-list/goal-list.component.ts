fb522cb4f4d8044b1942bc5c129cde56321b93bc
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 20:07:39 -0600

    Get goals from goalService

--- src/app/goals/goal-list/goal-list.component.ts
+++ src/app/goals/goal-list/goal-list.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Goal } from '../goal.model';
+import { GoalService } from '../goal.service';
 
 @Component({
   selector: 'app-goal-list',
   templateUrl: './goal-list.component.html',
 })
-export class GoalListComponent {}
+export class GoalListComponent implements OnInit {
+  goals: Goal[];
+
+  constructor(private goalService: GoalService) {}
+
+  ngOnInit() {
+    this.goals = this.goalService.getGoals();
+  }
+}
