5540d6e7647d595d24ea3c2c6727cfaa551997b7
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 20:07:00 -0600

    Make goal and index input properties

--- src/app/goals/goal-list/goal-item/goal-item.component.ts
+++ src/app/goals/goal-list/goal-item/goal-item.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Goal } from '../../goal.model';
 
 @Component({
   selector: 'app-goal-item',
   templateUrl: './goal-item.component.html',
 })
-export class GoalItemComponent {}
+export class GoalItemComponent {
+  @Input() goal: Goal;
+  @Input() index: number;
+}
