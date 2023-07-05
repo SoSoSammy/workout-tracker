97193d9de375d17175af2c7acc2521dae9f6c37d
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 20:06:05 -0600

    Create goal-detail.component.ts

---
+++ src/app/goals/goal-detail/goal-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { Goal } from '../goal.model';
+import { GoalService } from '../goal.service';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+})
+export class GoalDetailComponent implements OnInit {
+  goal: Goal;
+  id: number;
+  subscription: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private goalService: GoalService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.goal = this.goalService.getGoal(this.id);
+    });
+  }
+
+  onDeleteGoal() {}
+}
