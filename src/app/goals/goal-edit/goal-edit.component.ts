56eb82947ba3d9f4a266ae1146d57d0f99caa57e
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-04 20:06:24 -0600

    Set editMode to false, create onSubmit method

--- src/app/goals/goal-edit/goal-edit.component.ts
+++ src/app/goals/goal-edit/goal-edit.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-goal-edit',
   templateUrl: './goal-edit.component.html',
 })
-export class GoalEditComponent {}
+export class GoalEditComponent {
+  editMode = false;
+
+  onSubmit(goalForm: NgForm) {
+    console.log(goalForm.value);
+  }
+}
