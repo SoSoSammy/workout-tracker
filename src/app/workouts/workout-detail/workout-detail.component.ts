b5fc7e203df185ea0bc4997b6b6c9d5e4331ca23
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-10-03 16:11:07 -0600

    Add comments, refactor transformUrlsToEmbedUrls()

--- src/app/workouts/workout-detail/workout-detail.component.ts
+++ src/app/workouts/workout-detail/workout-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { Workout } from '../workout.model';
 import { WorkoutService } from '../workout.service';
@@ -10,10 +10,18 @@ import { WorkoutService } from '../workout.service';
   templateUrl: './workout-detail.component.html',
 })
 export class WorkoutDetailComponent implements OnInit {
-  workout: Workout;
-  id: number;
-  videoUrls;
+  workout: Workout; // The current workout
+  id: number; // The id of the current workout
+  videoUrls: SafeResourceUrl[]; // An array of YouTube embed URLs
 
+  /**
+   * Builds the workout detail component with its necessary services.
+   *
+   * @param workoutService - the workout service
+   * @param route - the current route
+   * @param router - the router
+   * @param sanitizer - the DOM sanitizer (used on links)
+   */
   constructor(
     private workoutService: WorkoutService,
     private route: ActivatedRoute,
@@ -21,18 +29,31 @@ export class WorkoutDetailComponent implements OnInit {
     private sanitizer: DomSanitizer
   ) {}
 
+  /**
+   * Subscribes to changes in the URL parameters and sets the current id and workout to
+   * reflect the url parameters. Calls the transformUrlsToEmbedUrls() method to transform
+   * YouTube video URLs to a YouTube embed URL.
+   */
   ngOnInit() {
     // Subscribe to changes in URL parameters
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
       this.workout = this.workoutService.getWorkout(this.id);
-      this.transformUrlsToEmbedUrls();
+      this.videoUrls = this.transformUrlsToEmbedUrls(this.workout.videos);
     });
   }
 
-  // Change YouTube urls to iframe embed urls
-  transformUrlsToEmbedUrls() {
-    this.videoUrls = this.workout.videos.map((url) => {
+  /**
+   * Takes an array of YouTube video URLs, removes the URL parameters, and formats the URLs as
+   * YouTube embed URLs.
+   *
+   * @param videos - the array of YouTube video URLs
+   * @returns the transformed array of video URLs, with URL parameters removed and formatted as
+   * a YouTube embed URL
+   */
+  transformUrlsToEmbedUrls(videos: string[]) {
+    // Create a new array of video URLs
+    let videoUrls = videos.map((url) => {
       let videoId = url.slice(32);
       // Get rid of URL parameters in URL
       if (videoId.includes('&')) {
@@ -44,8 +65,14 @@ export class WorkoutDetailComponent implements OnInit {
         `https://www.youtube.com/embed/${videoId}`
       );
     });
+
+    // Return the newly created video urls
+    return videoUrls;
   }
 
+  /**
+   * Deletes the current workout and navigates to main workouts page.
+   */
   onDeleteWorkout() {
     this.workoutService.deleteWorkout(this.id);
     this.router.navigate(['/workouts']);
