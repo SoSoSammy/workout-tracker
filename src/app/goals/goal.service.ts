d24d44bd8034b6d515f4c08a8efe5e2700e3482b
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-10-16 07:51:26 -0600

    Add comments

--- src/app/goals/goal.service.ts
+++ src/app/goals/goal.service.ts
@@ -5,7 +5,7 @@ import { Goal } from './goal.model';
 
 @Injectable({ providedIn: 'root' })
 export class GoalService {
-  goalsChanged = new Subject<Goal[]>();
+  goalsChanged = new Subject<Goal[]>(); // For when the goals are updated
 
   private goals = [
     new Goal('Health Goal', '2023-01-01', '', 'Weigh 150 pounds'),
@@ -19,10 +19,21 @@ export class GoalService {
     new Goal('Fitness Goal', '2021-07-01', '2023-05-03', 'Perform 1 pushup'),
   ];
 
+  /**
+   * Gets the goals.
+   *
+   * @returns a copy of the goals array
+   */
   getGoals() {
     return this.goals.slice();
   }
 
+  /**
+   * Gets a specified goal.
+   *
+   * @param index - the index of the goal
+   * @returns the goal with the specified index
+   */
   getGoal(index: number) {
     return this.goals[index];
   }
