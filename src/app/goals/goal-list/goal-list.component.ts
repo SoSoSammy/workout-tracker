18491cb4d377c3d1a3de3a8a74287a681726d499
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-10-16 07:51:33 -0600

    Add comments

--- src/app/goals/goal-list/goal-list.component.ts
+++ src/app/goals/goal-list/goal-list.component.ts
@@ -8,10 +8,19 @@ import { GoalService } from '../goal.service';
   templateUrl: './goal-list.component.html',
 })
 export class GoalListComponent implements OnInit {
-  goals: Goal[];
+  goals: Goal[]; // The goals
 
+  /**
+   * Builds the goal list component with its necessary services.
+   *
+   * @param goalService - the goal service
+   */
   constructor(private goalService: GoalService) {}
 
+  /**
+   * Initializes the goal list component with the goals from the
+   * goal service.
+   */
   ngOnInit() {
     this.goals = this.goalService.getGoals();
   }
