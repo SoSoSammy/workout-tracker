d5f52fe765d7d8b4f5f356b73b1c8c21c3f74a64
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2025-07-19 20:37:49 -0600

    Change template to template URL

--- src/app/workouts/workout-start/workout-start.component.ts
+++ src/app/workouts/workout-start/workout-start.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-workout-start',
-  template: '<p>No workout selected!</p>',
+  templateUrl: './workout-start.component.html',
 })
 export class WorkoutStartComponent {}
